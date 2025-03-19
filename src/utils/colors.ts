21e0103a1e6d5cb09cd8514c90372f160c9e4011
Bruno Pires <156872746+brunopirz@users.noreply.github.com>
2025-03-19 00:14:52 -0300

    Create colors.ts

---
+++ src/utils/colors.ts
@@ -0,0 +1,9 @@
+export const LIST_COLORS = [
+  { name: 'Blue', hex: '#0078D4' },
+  { name: 'Green', hex: '#107C10' },
+  { name: 'Purple', hex: '#5C2D91' },
+  { name: 'Pink', hex: '#E3008C' },
+  { name: 'Orange', hex: '#D83B01' },
+  { name: 'Red', hex: '#A80000' },
+  { name: 'Teal', hex: '#038387' }
+];
